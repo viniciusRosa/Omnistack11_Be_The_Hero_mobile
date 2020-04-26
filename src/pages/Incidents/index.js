83c6f2e6c6fcbed4afbef93368c2bacb0a77a96f
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-04-25 22:05:38 -0300

    initial commit

---
+++ src/pages/Incidents/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import LogoImg from '../../assets/logo.png'
+
+export default function Incidents() {
+    return (
+        <View>
+
+        </View>
+    )
+}
